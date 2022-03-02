547228d0977f415aabab305d3cd24c803e6d8062
teome <domkelly1010@gmail.com>
2022-03-02 17:37:46 +0000

    E3.9 Install cors

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const { application } = require('express')
 const express = require('express')
 const { json } = require('express/lib/response')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 // app.use(morgan('tiny'))
 morgan.token('reqbody', (req, res) => {
