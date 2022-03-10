41cf40bd8de5550f34034d73698a60302472946a
teome <domkelly1010@gmail.com>
2022-03-10 18:02:08 +0000

    E3.20 Implement custom validation on phonenumber

--- models/person.js
+++ models/person.js
@@ -21,7 +21,12 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    required: true,
+    minLength: 8,
+    validate: {
+      validator: v => /^\d{2,3}-\d+$/.test(v),
+      message: props => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, 'User phone number required'],
   },
 })
 
